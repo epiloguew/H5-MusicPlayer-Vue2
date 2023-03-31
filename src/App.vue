97a3e99bf1c71bc857c6e733048f2178fda4f9d5
epilogue <125470119+epiloguew@users.noreply.github.com>
2023-03-31 16:47:44 +0800

    feat：add component tab

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,18 @@
 <template>
     <div id="app">
         <my-head />
+        <my-tab />
         <router-view />
     </div>
 </template>
 <script>
 import MyHead from "@/components/head/index.vue";
+import MyTab from "@/components/tab/index.vue";
 export default {
     name: "index",
     components: {
         MyHead,
+        MyTab,
     },
 };
 </script>
@@ -17,5 +20,6 @@ export default {
 body {
     padding: 0;
     margin: 0;
+    background: #000;
 }
 </style>
---
+++ src/components/tab/index.vue
@@ -0,0 +1,32 @@
+<template>
+    <div class="tab-box">
+        <div v-for="item of arrList" :key="item.val">
+            {{ item.val }}
+        </div>
+    </div>
+</template>
+<script>
+export default {
+    name: "component-tab",
+    data() {
+        return {
+            arrList: [
+                { key: "recommend", val: "推荐" },
+                { key: "singer", val: "歌手" },
+                { key: "ranking", val: "排行" },
+                { key: "search", val: "搜索" },
+            ],
+        };
+    },
+    created() {},
+};
+</script>
+<style lang="less">
+.tab-box {
+    width: 100%;
+    display: flex;
+    justify-content: space-around;
+    padding: 14px 0;
+    color: rgba(255, 255, 255, 0.5);
+}
+</style>
